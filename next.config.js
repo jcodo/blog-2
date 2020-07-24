beefdf08287858a9232dea802118f9a6baf095d6
Jesse Sibley <inounomemynameisjesse@gmail.com>
2020-07-24 13:46:54 +0100

    Update feed

--- next.config.js
+++ next.config.js
@@ -7,14 +7,12 @@ module.exports = withMdxEnhanced({
 })({
   // Copied from https://github.com/tailwindlabs/blog.tailwindcss.com/blob/master/next.config.js
   webpack: (config, options) => {
-    if (!options.dev && options.isServer) {
-      const originalEntry = config.entry
+    const originalEntry = config.entry
 
-      config.entry = async () => {
-        const entries = { ...(await originalEntry()) }
-        entries['./scripts/build-rss.js'] = './scripts/build-rss.ts'
-        return entries
-      }
+    config.entry = async () => {
+      const entries = { ...(await originalEntry()) }
+      entries['./scripts/build-rss.js'] = './scripts/build-rss.ts'
+      return entries
     }
 
     return config
