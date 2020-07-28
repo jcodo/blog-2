2f732cabbd6dc46ff57657dbc9a59fd29d43f1e4
Jesse Sibley <inounomemynameisjesse@gmail.com>
2020-07-28 13:25:06 +0100

    Add fathom analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,31 @@ import '../css/index.css'
 import { AppProps } from 'next/app'
 import Footer from '../components/footer'
 
+import * as Fathom from 'fathom-client'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
+  useEffect(() => {
+    function onRouteChangeComplete() {
+      Fathom.trackPageview()
+    }
+
+    if (process.env.NODE_ENV === 'production') {
+      Fathom.load('GLCOKXMM', {
+        includedDomains: ['https://jessesibley.com'],
+      })
+
+      router.events.on('routeChangeComplete', onRouteChangeComplete)
+
+      return () => {
+        router.events.off('routeChangeComplete', onRouteChangeComplete)
+      }
+    }
+  }, [])
+
   return (
     <>
       <Component {...pageProps} />
